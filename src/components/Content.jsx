12e16636da47ca89a22e60a01eff84c9a224a43c
Vaibhav <vaibhavshrivastav222@gmail.com>
2022-12-13 23:07:17 +0530

    changes

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -14,13 +14,13 @@ export default function Content() {
             </p>
             <br />
             <br />
-            <button className="btn btn-outline-secondary">Explore Marketplace</button>
+            <button className="btn btn-primary">Explore Marketplace</button>
             <br />
             <br />
             <div className="row text-center">
-                <div className="col box shadow mx-3 p-3 mb-5 bg-white rounded"><img src="" alt="" /><br /> <button className="eth text-dark bg-light btn btn-outline-success"><box-icon className='mx-5' name='money-withdraw'></box-icon>44.02ETH &nbsp; &nbsp; &nbsp;<button className="btn btn-warning">Place a bid</button></button></div>
-                <div className="col box shadow mx-3 p-3 mb-5 bg-white rounded"><img src="" alt="" /><br /> <button className="eth text-dark bg-light btn btn-outline-success"><box-icon className='mx-5' name='money-withdraw'></box-icon>44.02ETH &nbsp; &nbsp; &nbsp;<button className="btn btn-warning">Place a bid</button></button></div>
-                <div className="col box shadow mx-3 p-3 mb-5 bg-white rounded"><img src="" alt="" /><br /> <button className="eth text-dark bg-light btn btn-outline-success"><box-icon className='mx-5' name='money-withdraw'></box-icon>44.02ETH &nbsp; &nbsp; &nbsp;<button className="btn btn-warning">Place a bid</button></button></div>
+                <div className="col box shadow mx-3 p-3 mb-5 bg-white rounded"><div className='hide'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi ut voluptas odit facilis consectetur ipsum cum doloremque beatae libero, quasi corrupti. Facere voluptatem ex reiciendis veritatis ad? A, voluptatum. Inventore?</div><br /> <button className="eth text-dark bg-light btn btn-outline-success"><box-icon className='mx-5' name='money-withdraw'></box-icon>44.02ETH &nbsp; &nbsp; &nbsp;<button className="btn btn-warning">Place a bid</button></button></div>
+                <div className="col box shadow mx-3 p-3 mb-5 bg-white rounded"><div className="hide">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis nihil commodi nulla dolorem error doloribus cupiditate animi quidem explicabo beatae. Quam aliquid dignissimos quae quibusdam ullam. Deleniti eveniet pariatur corrupti.</div><br /> <button className="eth text-dark bg-light btn btn-outline-success"><box-icon className='mx-5' name='money-withdraw'></box-icon>44.02ETH &nbsp; &nbsp; &nbsp;<button className="btn btn-warning">Place a bid</button></button></div>
+                <div className="col box shadow mx-3 p-3 mb-5 bg-white rounded"><div className="hide">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias, sapiente maxime nesciunt dolore est voluptatum omnis assumenda, voluptas architecto minima, culpa totam adipisci voluptatem officia sequi expedita! Dolorem, dolor asperiores.</div><br /> <button className="eth text-dark bg-light btn btn-outline-success"><box-icon className='mx-5' name='money-withdraw'></box-icon>44.02ETH &nbsp; &nbsp; &nbsp;<button className="btn btn-warning">Place a bid</button></button></div>
 
             </div>
         </div>
